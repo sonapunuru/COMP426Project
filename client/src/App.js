a1fe82a4cf421f7b9eae6e963dedd32215fe7a7a
XimenaColopy <43392764+XimenaColopy@users.noreply.github.com>
2023-04-18 00:50:36 -0400

    x1 rocks

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
